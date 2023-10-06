c6bd794c130a607a76d6d4e5a9a6ff9109f0b2a0
Ivy7N <ivy.ngatia@student.moringaschool.com>
2023-10-06 18:26:29 +0300

    complete dom lab

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Write your code here!
+const mainNode = document.getElementById('main');
+
+if (mainNode) {
+  mainNode.remove();
+}
+const newHeader = document.createElement('h1');
+newHeader.id = 'victory';
+
+newHeader.textContent = 'YOUR-NAME is the champion';
+
+document.body.appendChild(newHeader);
+
